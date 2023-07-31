b8541e11a283fa46d51646371f43e3c0b72e88e2
HasanboyXomidov <hasanboyxomidov04@gmail.com>
2023-07-31 10:22:06 +0500

    Fixed errors and omissions in the USERAUTH section

--- test/LoadSeeker.UniTest/ValidatorTests/Cargos/CargoCreateValidatorTests.cs
+++ test/LoadSeeker.UniTest/ValidatorTests/Cargos/CargoCreateValidatorTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CargoSeeker.Service.Validators;
+using CargoSeeker.Service.Validators.CargosCreateValidators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +9,16 @@ using System.Threading.Tasks;
 namespace LoadSeeker.UniTest.ValidatorTests.Cargos;
 
 public class CargoCreateValidatorTests
-{    
+{
+    [Theory]
+    [InlineData("fruits")]
+    [InlineData("vegetables")]
+    [InlineData("iron")]
+    [InlineData("Truncs")]
+    [InlineData("Cars")]
+    [InlineData("Working Tools")]
+    [InlineData("Animals")]
+    public void ShouldReturnCorrectCargoName(string cargoName)
+    {        
+    }
 }
